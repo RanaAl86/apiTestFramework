d29638f7c4801645d3ecd9c2f41ae96a13acd0e6
RanaAlDarraji <rana.aldarraji@microhealth.com>
2024-08-26 12:24:09 -0400

    all_in_one update

--- src/test/java/com/examples/tests/platzi/TC_04.java
+++ src/test/java/com/examples/tests/platzi/TC_04.java
@@ -1,6 +1,8 @@
 package com.examples.tests.platzi;
 
 import com.examples.utilities.PlatziUrl;
+import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -10,12 +12,18 @@ public class TC_04 extends PlatziUrl {
     @Test
     public void testDeleteProduct() {
 
-        given().pathParam("id", 145)
+        Response response = given().pathParam("id", 90)
                 .when()
                 .delete("/products/{id}")
                 .then()
                 .statusCode(200)
                 .extract()
-                .response().prettyPrint();
+                .response();
+
+        String responseBody = response.getBody().asString();
+        System.out.println("Response Body: " + responseBody);
+
+        Assert.assertEquals(responseBody.trim(),"true");
+
     }
 }
